b1ef493628fd244b4c49487ebd5a95f7ad20d897
Maksim Butin <maksim.butin@e-legion.com>
2022-07-07 00:38:36 +0700

    Refactor go backend

--- server.go
+++ server.go
@@ -6,6 +6,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var teams = []Team{
+	{1, "3CRABS"},
+	{2, "CRABS"},
+	{3, "C"},
+}
+
 type Team struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
@@ -13,20 +19,14 @@ type Team struct {
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.GET("/teams", getTeams)
+	e.GET("/teams", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, teams)
+	})
 
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func getTeams(c echo.Context) error {
-	teams := []Team{
-		{1, "3CRABS"},
-		{2, "CRABS"},
-		{3, "C"},
-	}
-	return c.JSON(http.StatusOK, teams)
 }
